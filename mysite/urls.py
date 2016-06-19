a3bc5e06a2f78ad493d8808ab88424ee6b1b32cb
squeaksqueak <paulinakociemska@gmail.com>
2016-06-19 16:23:50 +0200

    Komentarze

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+import django.contrib.auth.views
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', django.contrib.auth.views.login, name = 'login'),
+    url(r'^accounts/logout/$', django.contrib.auth.views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('blog.urls')),
+    
 ]
 
  
