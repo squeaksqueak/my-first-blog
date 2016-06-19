accaeaa36bed82a95547a3aa2ed7e6af843f8084
squeaksqueak <paulinakociemska@gmail.com>
2016-06-19 10:18:11 +0200

    My Django Girls app, first commit

---
+++ start.py
@@ -0,0 +1,32 @@
+# def dodawanko(liczba):
+# 	if liczba > 0:
+# 		print(liczba+2)
+# 	else:
+# 		print(":P")
+
+# dodawanko(-1)
+
+# def hej(imie):
+#     print('Hej ' + imie + '!')
+
+# dziewczyny = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'Ty']
+# for imie in dziewczyny:
+#     hej(imie)
+#     print('Kolejna dziewczyna')
+
+
+# def hej(literka):
+#     print('Hej ' + literka + '!')
+
+# literki = ['a', 'b', 'c'] 
+# for literka in literki:
+#     hej(literka)
+#     print('Kolejna literka!')
+piesek = {'imie' : "Poldek", 'rasa' : "kundelek", 'lapy' : [4], 'ogon' : [1], 'siersc' : 'dluga', 'przysmaki' : 'wiele', 'wiek' : [2]}
+
+
+def psieimie(pies):
+    print(pies['imie'])
+
+psieimie(pies=piesek)
+
