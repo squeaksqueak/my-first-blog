ef942f954e5348b910196a50865f367b7c35199b
squeaksqueak <paulinakociemska@gmail.com>
2016-06-19 17:30:04 +0200

    Modyfikacja szablonu, obrazki

---
+++ blog/migrations/0003_post_image.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0002_comment'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='post',
+            name='image',
+            field=models.CharField(default='', max_length=200),
+            preserve_default=False,
+        ),
+    ]
